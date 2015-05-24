9a083117fce2da66c295b0cd54e0fa6745c356ca
canibal <jose_canibal@hotmail.com>
2015-05-23 20:46:15 -0600

    Matiz de equivalencias para consulta externa al SIAI

---
+++ VistaMatrizWeb.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<html>
+<head>
+</head>
+<body>
+<div class="container">
+    <center>
+        <h1>Seleccione su Instituci&oacute;n y Carrera de procedencia</h1>
+    </center>
+    <iframe src="siai_administrativo/Equivalencias/DataTables/MatrizVista.php" height="700px" width="100%" border="0"  frameborder="0" ></iframe></center><!-- InstanceEndEditable --></div>
+    <!--<footer> <strong>Universidad Politécnica de El Salvador <?php echo date('Y');?> </strong></footer>-->
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+<!-- InstanceEnd --></html>
